2f86e2192838f8f254e1013ebbf259319d3db033
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-05-03 09:49:35 +0530

    Solved DSA question: Paranthesis Checker

---
+++ paranthesis_checker.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        if(x.length() == 1)
+            return false;
+            
+        stack<char>st;
+        
+        for(int i=0;i<x.length();i++){
+            if(x[i] == '(' || x[i] == '{' || x[i] == '[')
+                st.push(x[i]);
+                
+            else if(x[i] == ')'){
+                if(st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+            
+            else if(x[i] == ']'){
+                if(st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+            }
+            
+            else if(x[i] == '}'){
+                if(st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+            }
+        }
+    return st.empty();
+    }
+
+};
+
+int main(){
+
+return 0;
+}
